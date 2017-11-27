4ffb1d17471b0916254caff1c20575a53debe293
Nic Schumann <nicolas_schumann@brown.edu>
2017-11-26 19:34:06 -0500

    [Config] increased polling interval.

--- glow-io/index.js
+++ glow-io/index.js
@@ -31,7 +31,7 @@ var GlowNodeIO = function( server ) {
      *
      * TODO: make this a more general parameter that can be independently configured.
      */
-    self.calibration_period = 1000;
+    self.calibration_period = 5000;
 
 
     self.server = server;
