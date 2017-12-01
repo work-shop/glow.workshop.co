a441d03bcbd5e4a45bef25555e5b65e88ba3e4a3
Nic Schumann <nicolas_schumann@brown.edu>
2017-12-01 14:31:36 -0500

    [Oscillators] added a discrete oscillator for testing

--- glow-state/index.js
+++ glow-state/index.js
@@ -77,7 +77,7 @@ var GlowNodeState = function( config, log ) {
      * @param t the current uptime of the system. This is used to produce the oscillation.
      * @return Object { r: Int, g: Int, b: Int }. Oscillators for R, G, and B, PWM, given the current network state.
      */
-    GlowNodeState.prototype.getOscillators = require('./oscillators-C.js')( self );
+    GlowNodeState.prototype.getOscillators = require('./oscillators-discrete.js')( self );
 
     /**
      * NOTE: Try hardcoding the valid IPs, for now.
---
+++ glow-state/oscillators-discrete.js
@@ -0,0 +1,136 @@
+'use strict';
+
+const linearmap = require('./linear-map.js');
+
+const a = 0.85;
+
+const f = function( k, n ) {
+    return Math.PI * a * ( k + 1 ) / n;
+};
+
+module.exports = function( self ) {
+
+    const active_map = linearmap( 0, 1 )( 0, self.config.hardware.PWM.MAX_INTERVAL );
+    const passive_map = linearmap( 0, 2 )( 0, self.config.hardware.PWM.MAX_INTERVAL );
+
+    return function( t ) {
+
+        const state = self.get();
+
+        const s = parseInt( self.local_state );
+
+        const k = state.reduce( function( b,a ) { return b + a.state; }, 0) - s;
+
+        const n = state.length;
+
+        const k_n = (k + s) / n;
+
+        if ( s === 1 ) {
+
+            return {
+
+                r: parseInt( Math.floor( active_map( 1 ) ) ),
+                g: parseInt( Math.floor( active_map( 1 ) ) ),
+                b: parseInt( Math.floor( active_map( 1 ) ) )
+
+            };
+
+        } else {
+
+            let omega = f( k+s, n );
+
+            if ( k_n < 1/6 ) {
+
+                let A_b = 0.5;
+
+                return {
+
+                    r: 0,
+                    g: 0,
+                    b: parseInt( Math.floor( passive_map( A_b * Math.sin( omega * t ) + A_b ) ) )
+
+                };
+
+            } else if ( k_n < 1/3 ) {
+
+                let A_b = 0.75;
+                let A_y = 0.1;
+
+                return {
+
+                    r: parseInt( Math.floor( passive_map( A_y * Math.sin( omega * t ) + A_y ) ) ),
+                    g: parseInt( Math.floor( passive_map( A_y * Math.sin( omega * t ) + A_y ) ) ),
+                    b: parseInt( Math.floor( passive_map( A_b * Math.sin( omega * t ) + A_b ) ) )
+
+                };
+
+            } else if ( k_n < 1/2 ) {
+
+                let A_b = 0.6;
+                let A_y = 0.2;
+
+                return {
+
+                    r: parseInt( Math.floor( passive_map( A_y * Math.sin( omega * t ) + A_y ) ) ),
+                    g: parseInt( Math.floor( passive_map( A_y * Math.sin( omega * t ) + A_y ) ) ),
+                    b: parseInt( Math.floor( passive_map( A_b * Math.sin( omega * t ) + A_b ) ) )
+
+                };
+
+            } else if ( k_n < 2/3 ) {
+
+                let A_b = 0.1;
+                let A_y = 0.7;
+
+                return {
+
+                    r: parseInt( Math.floor( passive_map( A_y * Math.sin( omega * t ) + A_y ) ) ),
+                    g: parseInt( Math.floor( passive_map( A_y * Math.sin( omega * t ) + A_y ) ) ),
+                    b: parseInt( Math.floor( passive_map( A_b * Math.sin( omega * t ) + A_b ) ) )
+
+                };
+
+            } else if ( k_n < 5/6 ) {
+
+                let A_b = 0.2;
+                let A_y = 0.9;
+
+                return {
+
+                    r: parseInt( Math.floor( passive_map( A_y * Math.sin( omega * t ) + A_y ) ) ),
+                    g: parseInt( Math.floor( passive_map( A_y * Math.sin( omega * t ) + A_y ) ) ),
+                    b: parseInt( Math.floor( passive_map( A_b * Math.sin( omega * t ) + A_b ) ) )
+
+                };
+
+            } else if ( k_n < 1 ) {
+
+                let A_b = 0.5;
+                let A_y = 0.9;
+
+                return {
+
+                    r: parseInt( Math.floor( passive_map( A_y * Math.sin( omega * t ) + A_y ) ) ),
+                    g: parseInt( Math.floor( passive_map( A_y * Math.sin( omega * t ) + A_y ) ) ),
+                    b: parseInt( Math.floor( passive_map( A_b * Math.sin( omega * t ) + A_b ) ) )
+
+                };
+
+            } else {
+
+                self.log.write('warning', 'state', '?? s =/= 1, but k/n >= 1 ??');
+
+                return {
+
+                    r: parseInt( Math.floor( active_map( 1 ) ) ),
+                    g: parseInt( Math.floor( active_map( 1 ) ) ),
+                    b: parseInt( Math.floor( active_map( 1 ) ) )
+
+                };
+
+            }
+
+        }
+
+    };
+};
